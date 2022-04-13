f0cf3d16522b851efa70894b2e1b5a228a4a25bd
superSCW <1016733717@qq.com>
2022-04-13 23:16:53 +0800

    Add files via upload
    提交代码

---
+++ myFramework.framework/Headers/PublicFile.h
@@ -0,0 +1,16 @@
+//
+//  PublicFile.h
+//  myFramework
+//
+//  Created by scw on 2022/4/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PublicFile : NSObject
++(void)testZip;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ myFramework.framework/Headers/myFramework.h
@@ -0,0 +1,18 @@
+//
+//  myFramework.h
+//  myFramework
+//
+//  Created by scw on 2022/4/13.
+//
+
+#import <Foundation/Foundation.h>
+#import "PublicFile.h"
+//! Project version number for myFramework.
+FOUNDATION_EXPORT double myFrameworkVersionNumber;
+
+//! Project version string for myFramework.
+FOUNDATION_EXPORT const unsigned char myFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myFramework/PublicHeader.h>
+
+
